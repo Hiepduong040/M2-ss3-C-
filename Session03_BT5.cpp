5bba4a61cee2e508e2cde61ffbebc2a3f1d6307c
Hiepduong040 <147359314+Hiepduong040@users.noreply.github.com>
2023-11-09 08:52:13 +0700

    Add files via upload

--- Session03_BT5.cpp
+++ Session03_BT5.cpp
@@ -1,5 +1,11 @@
-#include <stdio.h>
+#include<stdio.h>
 int main(){
-	
-	
+	int number,sum=0,inverse=0;
+	printf("Nhap vao so nguyen co 4 chu so: ");
+	scanf("%d", &number);
+	sum=(number%10)+((number/10)%10)+((number/100)%10)+(number/1000);
+	inverse=(number%10)*1000+((number/10)%10)*100+((number/100)%10)*10+(number/1000);
+	printf("Tong 4 chu so:%d\n",sum);
+	printf("So nghich dao:%d\n",inverse); 
 }
+
